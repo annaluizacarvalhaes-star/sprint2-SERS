6d1fd922a2442cf41ed1baae9a96e7732fd385fe
annaluizacarvalhaes-star <annaluizacarvalhaes@gmail.com>
2026-06-11 09:53:37 -0300

    iniciando o código

---
+++ simulacao.py
@@ -0,0 +1,13 @@
+# SIMULAÇÃO SISTEMA DE CARREGAMENTO INTELIGENTE FOTOVOLTAICO - GOODWE
+from fontTools.misc.cython import returns
+
+print("-"*60)
+print ("SIMULAÇÃO SISTEMA DE CARREGAMENTO INTELIGENTE FOTOVOLTAICO - GOODWE")
+print("-"*60)
+
+print("\n")
+bat_atual = input("Digite a Bateria atual: ")
+bat_desejada = input("Digite a Bateria desejada: ")
+
+print("MENU")
+print(f"Estado Inicial do Veículo: {bat_atual}% | Meta Desejada: {bat_desejada}%")
